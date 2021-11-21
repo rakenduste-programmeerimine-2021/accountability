cde4ab23fccf35da16792ffbe0b4388c5945a21b
Andres Naris <andres.naris@icefire.ee>
2021-11-21 21:43:57 +0200

    front page, ant library and login page

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import { Layout, Menu } from 'antd';
+import Login from './pages/login'
+const { Header, Content, Footer } = Layout;
+const centerStyle = {
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'right',
+}
+
+
+const App = () => (
+  <div className="app" >
+  <Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={centerStyle} >
+        <Menu.Item key="1">My calls</Menu.Item>
+        <Menu.Item key="2">Book a call</Menu.Item>
+        <Menu.Item key="3">Call history</Menu.Item>
+        <Menu.Item key="4">Notifications</Menu.Item>
+        <Menu.Item key="5">My account</Menu.Item>
+        < div className="items" />
+      </Menu>
+    </Header>
+    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+      <div className="site-layout-background" style={{ padding: 24, minHeight: 570 }}>
+        Test your webcam! 
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Rakenduste programmeerimine Accountability 2021</Footer>
+  </Layout>
+
+  </div>
+);
+
+export default App;
