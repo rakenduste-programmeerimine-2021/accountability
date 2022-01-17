7625a333e49f4ab3b4cb7a45f87f48bf6761157d
Andres Naris <andres.naris@icefire.ee>
2022-01-17 19:04:39 +0200

    fresh start

--- backend-node/src/routes/post.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const postController = require("../controllers/post")
-
-router.get("/", postController.getPosts)
-router.post("/create", postController.createPost)
-router.put("/update/:id", postController.updatePost)
-router.delete("/delete/:id", postController.deletePost)
-
-module.exports = router
--- frontend/src/pages/Account.js
+++
@@ -1,9 +0,0 @@
-function Account() {
-    return (
-      <div>
-       This is an Account page
-      </div>
-    )
-  }
-  
-  export default Account;
